81b8528529e1af24055d87eb58537129027371f2
_Pear <pearofficialyt@gmail.com>
2020-05-11 10:22:42 -0400

    Changed invalid message to match with others

--- WaterstonBotv1/commands/moderation/warn.js
+++ WaterstonBotv1/commands/moderation/warn.js
@@ -7,7 +7,7 @@ module.exports = {
   description: "Warns a user for a reason",
   run: async (client, message, args) => {
     if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
-    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ Invalid user`).then(r => r.delete({
+    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
       timeout: 10000
     }))
     let member = message.mentions.members.first()
