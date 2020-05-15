9324316141ce60baab61339ef7bda549e5c1a9a8
_Pear <pearofficialyt@gmail.com>
2020-05-15 18:05:26 -0400

    Fix path once again - confirmed working

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -5,10 +5,10 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-    readdirSync("./src/commands/").forEach(dir => {
+    readdirSync(`./src/commands/`).forEach(dir => {
         const commands = readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
-            let pull = require(`../src/commands/${dir}/${file}`);
+            let pull = require(`../commands/${dir}/${file}`);
     
             if (pull.name) {
                 client.commands.set(pull.name, pull);
