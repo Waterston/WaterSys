054142562abf84bbbd588159f0e6d973ea02f4ef
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-05-18 23:57:02 -0400

    maybe fixed it idk

---
+++ src/commands/public/suggest.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    name: "suggest",
+    category: "public",
+    description: "Suggest a V1 command",
+    usage: "<mention, id>",
+    run: async (client, message, args) => {
+        let rMember = args[0]       
+        const channel = message.guild.channels.cache.find(channel => channel.name === "suggestions")
+        
+        if (!channel)
+            return channel.send("Channel not found. Please contact a Founder to fix this.");
+            
+            const appealchannelembed = new Discord.MessageEmbed()
+            .setColor("#0084ff")
+            .setTimestamp()
+            .setAuthor(`WaterstonSystems`, client.user.displayAvatarURL())
+            .setFooter("WaterstonSystems", client.user.displayAvatarURL()) 
+            .setTitle("Suggestion Submitted")
+            .setDescription(`${message.member}, sucessfully sent your suggestion to further review with the development team!`)
+
+
+            const suggestionembed = new Discord.MessageEmbed()
+            .setColor("#0084ff")
+            .setTimestamp()
+            .setAuthor(`WaterstonSystems`, client.user.displayAvatarURL())
+            .setFooter("WaterstonSystems", client.user.displayAvatarURL()) 
+            .setTitle("New Suggestion")
+            .addField('Submitted by:', `${message.member}`)
+            .addField('Suggestion Type:', rMember)
+            .addField('Suggestion:', `${args.slice(1).join(" ")}`)
+            message.channel.send(appealchannelembed)
+            let msg = await channel.send(suggestionembed)
+              await msg.react('✅')
+              await msg.react('❌')
+            return
+    }
+}
--- src/commands/public/uptime.js
+++ src/commands/public/uptime.js
@@ -11,7 +11,7 @@ module.exports = {
             const min = Math.floor((ms / (1000 * 60)) % 60).toString()
             const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
             const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-            return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds, `
+            return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds `
         }
         
         
@@ -20,7 +20,7 @@ module.exports = {
         .setTimestamp()
         .setAuthor(`WaterstonSystems`, client.user.displayAvatarURL())
         .setFooter("WaterstonSystems", client.user.displayAvatarURL()) 
-        .setTitle("Bot Uptime")
+        .setTitle("WaterstonBotv1 Uptime")
         .setDescription(`${duration(client.uptime)}`)
         message.channel.send(uptimeembed)
     
