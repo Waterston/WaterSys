c9971aae8bbe95e857533694967c95569217e506
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-05-28 09:46:29 -0400

    Create whois.js

---
+++ src/commands/verification/whois.js
@@ -0,0 +1,21 @@
+let Discord = require('discord.js')
+let fetch = require('node-superfetch')
+
+module.exports = {
+    name: "whois",
+    category: "info",
+    description: "Returns command list",
+    run: async (client, message, args) => {
+        let response = fetch.get(`https://groups.roblox.com/v1/groups/5231364`)
+        const whoisembed = new Discord.MessageEmbed()
+        .setColor("#0084ff")
+        .setTimestamp()
+        .setAuthor(`WaterstonSystems`, client.user.displayAvatarURL())
+        .setFooter("WaterstonSystems", client.user.displayAvatarURL()) 
+        .setTitle("Roblox Information")
+        .setDescription(`${response.data.name}`)
+        message.channel.send(whoisembed)
+      }
+    }
+
+
