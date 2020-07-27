49f7fd0e7855918bb32477c3242005e15d511e5b
_Pear <pearofficialyt@gmail.com>
2020-07-27 01:47:06 -0400

    Update avatar.js

--- src/commands/public/avatar.js
+++ src/commands/public/avatar.js
@@ -7,12 +7,15 @@ module.exports = {
     description: "Returns the specified user's profile picture",
     usage: "<mention, id>",
     run: async (client, message, args) => {
-        let user = message.mentions.members.first()
+        let user = message.mentions.users.first() || message.author;
+        if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
+        timeout: 10000
+        }))
         const embed = new Discord.MessageEmbed()
         .setColor("#0084ff")
-        .setAuthor(message.user.id, message.user.displayAvatarURL())
+        .setAuthor(message.author.username, message.author.displayAvatarURL())
         .setTitle("Avatar")
-        .setImage(user.avatarURL)
+        .setImage(`${user.avatarURL}`, true)
         message.channel.send(embed)
     }
 }
