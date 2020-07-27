35336a27292a3f51c3a63c5a39ab2f04c9a2ba65
_Pear <pearofficialyt@gmail.com>
2020-07-27 01:53:40 -0400

    Update avatar.js

--- src/commands/public/avatar.js
+++ src/commands/public/avatar.js
@@ -8,14 +8,12 @@ module.exports = {
     usage: "<mention, id>",
     run: async (client, message, args) => {
         let user = message.mentions.users.first() || message.author;
-        if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
-        timeout: 10000
-        }))
         const embed = new Discord.MessageEmbed()
         .setColor("#0084ff")
         .setAuthor(message.author.username, message.author.displayAvatarURL())
         .setTitle("Avatar")
-        .setImage(`${user.avatarURL}`, true)
+        .setTimestamp()
+        .setImage(user.displayAvatarURL)
         message.channel.send(embed)
     }
 }
