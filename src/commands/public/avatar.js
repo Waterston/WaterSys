f45487dbf65b6d8e00ef5aba01280742b5bb6831
_Pear <pearofficialyt@gmail.com>
2020-07-27 02:03:54 -0400

    Update avatar.js

--- src/commands/public/avatar.js
+++ src/commands/public/avatar.js
@@ -13,7 +13,7 @@ module.exports = {
         .setAuthor(message.author.username, message.author.displayAvatarURL())
         .setTitle("Avatar")
         .setTimestamp()
-        .setImage(user.displayAvatarURL)
+        .setImage(`${user.displayAvatarURL}`, true)
         message.channel.send(embed)
     }
 }
