e5f32c6f80ae9783925092697da3a394a8b2ea88
_Pear <pearofficialyt@gmail.com>
2020-08-16 23:11:48 -0400

    Update mute.js

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -7,12 +7,12 @@ module.exports = {
     description: "Mutes a user",
     run: async (client, message, args) => {
         if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
+	if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
+      	timeout: 10000
+   	}))
+   	let member = message.mentions.members.first()
 
-        let mute = message.mentions.members.first() || message.guild.members.get(args[0])
-        if(!mutee) return message.channel.send(`⚠️ No user specified, please mention the user.`)
-
-
-        if (message.author.id === mutee.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
+        if (message.author.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
 
         let reason = args.slice(1).join(" ");
         if(!reason) reason = "No reason given"
@@ -25,7 +25,7 @@ module.exports = {
           dynamic: true
         }))
         .setTitle(`Mute Issued`)
-        .setDescription(`**User Muted:** <@${mutee.user.id}>\n**Content Moderator:** <@${message.author.id}>\n**Reason:** ${reason}`)
+        .setDescription(`**User Muted:** <@${member.user.id}>\n**Content Moderator:** <@${message.author.id}>\n**Reason:** ${reason}`)
         .setTimestamp()
         .setFooter(client.user.username, client.user.displayAvatarURL()) 
         let mutedlogembed = new Discord.MessageEmbed()
@@ -34,7 +34,7 @@ module.exports = {
           dynamic: true
         }))
         .setTitle(`Mute Issued`)
-        .setDescription(`Sucessfully issued a mute to <@${mutee.user.id}>.`)
+        .setDescription(`Sucessfully issued a mute to <@${member.user.id}>.`)
         .addFields(
 		  { name: 'Reason', value: `${reason}`, inline: true },
 		  { name: 'Moderator', value: `<@${message.author.id}>`, inline: true },
@@ -42,7 +42,7 @@ module.exports = {
         .setTimestamp()
         .setFooter(client.user.username, client.user.displayAvatarURL()) 
 
-        mutee.roles.add(muterole.id).catch(console.error).then(() => {
+        member.roles.add(muterole.id).catch(console.error).then(() => {
             message.channel.send(mutedlogembed)
             return client.channels.resolve('709074878912790529').send(mutedembed)
         })
