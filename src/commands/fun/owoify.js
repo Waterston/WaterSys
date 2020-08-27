5443284eec6a2d0a9f97568339466c151ec2a3fe
_Pear <pearofficialyt@gmail.com>
2020-08-27 19:28:40 -0400

    test

--- src/commands/fun/owoify.js
+++ src/commands/fun/owoify.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 const faces = ['(・`ω´・)', ';;w;;', 'OwO', 'owo', 'UwU', 'uwu', '>w<', '^w^']
 
 function Owoify (str) {
-  str = str.replace(/(?:r|l)/g, 'w')
-  str = str.replace(/(?:R|L)/g, 'W')
-  str = str.replace(/n([aeiou])/g, 'ny$1')
-  str = str.replace(/N([aeiou])/g, 'Ny$1')
-  str = str.replace(/N([AEIOU])/g, 'Ny$1')
-  str = str.replace(/ove/g, 'uv')
-  str = str.replace(/!+/g, ' ' + faces[Math.floor(Math.random() * faces.length)] + ' ')
+  str = String(str).replace(/(?:r|l)/g, 'w')
+  str = String(str).replace(/(?:R|L)/g, 'W')
+  str = String(str).replace(/n([aeiou])/g, 'ny$1')
+  str = String(str).replace(/N([aeiou])/g, 'Ny$1')
+  str = String(str).replace(/N([AEIOU])/g, 'Ny$1')
+  str = String(str).replace(/ove/g, 'uv')
+  str = String(str).replace(/!+/g, ' ' + faces[Math.floor(Math.random() * faces.length)] + ' ')
 
   return str
 };
@@ -18,7 +18,7 @@ module.exports = {
     name: "owoify",
     aliases: ['owo', 'uwu', 'furryspeak', 'whatsthis'],
     category: "fun",
-    description: "Run messages through the Owoify filter.",
+    description: "Run messages through the owoify filter.",
     usage: "<message>",
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
