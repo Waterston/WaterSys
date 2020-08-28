0c0f19a826b26bfc2c0f2be9a67016377d123039
_Pear <pearofficialyt@gmail.com>
2020-08-27 23:12:04 -0400

    Add rainbow flag to the message + show author field

--- src/commands/fun/gayrate.js
+++ src/commands/fun/gayrate.js
@@ -10,8 +10,9 @@ module.exports = {
         let member = message.mentions.members.first() || message.member
         const embed = new Discord.MessageEmbed()
         .setColor("#0084ff")
+        .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.displayAvatarURL())
         .setTitle(`Gay Rate`)
-        .setDescription(`<@${member.user.id}> is ${Math.floor(Math.random() * 101)}% gay.`)
+        .setDescription(`<@${member.user.id}> is ${Math.floor(Math.random() * 101)}% gay. :rainbow_flag:`)
         .setFooter(client.user.username, client.user.displayAvatarURL())
         .setTimestamp()
         message.channel.send(embed)
