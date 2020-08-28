f89a95218b1c42450061fce9c1f2595b48b2648d
_Pear <pearofficialyt@gmail.com>
2020-08-27 20:14:46 -0400

    Create shiba.js

---
+++ src/commands/fun/shiba.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+const fetch = require('node-fetch')
+
+module.exports = {
+    name: "shiba",
+    aliases: ['shibe', 'doge'],
+    category: "fun",
+    description: "Get a random picture of a Shiba Inu dog.",
+    run: async (client, message, args) => {
+        const body = await fetch('https://shibe.online/api/shibes').then(res => res.json())
+        const embed = new Discord.MessageEmbed()
+        .setColor("#0084ff")
+        .setImage(body[0])
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setTimestamp()
+        message.channel.send(embed)
+    }
+}
