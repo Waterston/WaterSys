7880f1de9bce6fea69258b2f719f6be95a2fa5a6
_Pear <pearofficialyt@gmail.com>
2020-08-28 00:20:38 -0400

    Update gayrate.js

--- src/commands/fun/gayrate.js
+++ src/commands/fun/gayrate.js
@@ -10,7 +10,7 @@ module.exports = {
         let member = message.mentions.members.first() || message.member
         const embed = new Discord.MessageEmbed()
         .setColor("#0084ff")
-        .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.displayAvatarURL())
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
         .setTitle(`Gay Rate`)
         .setDescription(`<@${member.user.id}> is ${Math.floor(Math.random() * 101)}% gay. :rainbow_flag:`)
         .setFooter(client.user.username, client.user.displayAvatarURL())
