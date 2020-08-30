dbf000f5aa2a593c022a82cdb215e1286bd87c84
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-08-30 16:34:05 -0400

    first one to say pear gets a hug from pear

---
+++ src/commands/subscribe/subscribehiring.js
@@ -0,0 +1,25 @@
+const { stripIndents } = require('common-tags');
+let Discord = require('discord.js')
+
+module.exports = {
+    name: "subscribe business",
+    category: "public",
+    description: "public",
+    run: async (client, message, args) => {
+        let role = message.guild.roles.cache.find(r => r.name === "Business Subscriber");
+
+        let embed = new Discord.MessageEmbed()
+        .setColor("#0084ff")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({
+          dynamic: true
+        }))
+        .setTitle(`Subscribed Business Announcements`)
+        .setDescription(`Sucessfully added you to Business Announcements.`)
+        .setTimestamp()
+        .setFooter(client.user.username, client.user.displayAvatarURL()) 
+
+        message.author.roles.add(role.id).catch(console.error).then(() => {
+            message.channel.send(embed)
+        })
+  }
+}; 
