58a0b2c0cc8fd7705b4e52fc2b29ccb5607de049
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-08-31 16:02:40 -0400

    s

---
+++ src/commands/verification/whois.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+const fetch = require('node-fetch')
+
+module.exports = {
+    name: "whois",
+    aliases: ['roblox', 'info'],
+    category: "Roblox",
+    description: "stalk ppl",
+    run: async (client, message, args) => {
+
+        //Get Response of Body
+        const body = await fetch(`https://verify.eryn.io/api/user/${message.author.id}`).then(res => res.json())
+
+        //Create Sending Embed Completion
+        const whoembed = new Discord.MessageEmbed()
+        .setTitle(`Roblox Username: ${body.robloxUsername}`)
+        .setDescription(`Fuck Pear too, all my homies hate Pear`)
+        .setColor("#0084ff")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setTimestamp()
+        message.channel.send(whoembed)
+    }
+}
