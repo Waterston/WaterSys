6c5cefefa71fa4b25425843783ea218523602bd2
_Pear <pearofficialyt@gmail.com>
2020-09-02 17:53:21 -0400

    Update links.js

--- src/commands/public/links.js
+++ src/commands/public/links.js
@@ -4,18 +4,18 @@ module.exports = {
     name: "links",
     aliases: ['link', 'invite', 'roblox'],
     category: "public",
-    description: "Retrieve information for the current server",
+    description: "Posts the most updated links for Waterston content",
     usage: "<mention, id>",
     run: async (client, message, args) => {
        if ( message.guild.id === '659451316707524618') { // Makes this command only work for the main server
         const embed = new Discord.MessageEmbed()
-        .setAuthor(message.guild.name, message.guild.iconURL())
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
         .setColor("#0084ff")
         .setTitle("Links")
-        .addField("Server", "N/A", true)
-        .addField("Roblox Group", "https://www.roblox.com/groups/5231364/", true)
-        .addField("Website", "N/A", true)
-        .addField("Bot Invite", "N/A", true)
+        .addField("Server", "https://dicord.io/waterston", false)
+        .addField("Roblox Group", "https://www.roblox.com/groups/5231364/", false)
+        .addField("Website", "Coming soon", false)
+        .addField("Bot Invite", "Coming soon", false)
         .setFooter(client.user.username, client.user.displayAvatarURL())
         .setTimestamp()
         message.channel.send(embed)
