ff0b07621cfb370a988d66e05cad80201eb5f2a4
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-09-09 22:05:16 -0400

    stuff

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -1,50 +1,34 @@
 const { stripIndents } = require('common-tags');
 let Discord = require('discord.js')
+let ms = require('ms')
 
 module.exports = {
     name: "mute",
     category: "moderation",
     description: "Mutes a user",
     run: async (client, message, args) => {
-        if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
-	if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
-      	timeout: 10000
-   	}))
-   	let member = message.mentions.members.first()
-
-        if (message.author.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
-
-        let reason = args.slice(1).join(" ");
-        if(!reason) reason = "No reason given"
-
-        let muterole = message.guild.roles.cache.find(r => r.name === "Muted");
-
-        let mutedembed = new Discord.MessageEmbed()
-        .setColor("#0084ff")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({
-          dynamic: true
-        }))
-        .setTitle(`Mute Issued`)
-        .setDescription(`**User Muted:** <@${member.user.id}>\n**Content Moderator:** <@${message.author.id}>\n**Reason:** ${reason}`)
-        .setTimestamp()
-        .setFooter(client.user.username, client.user.displayAvatarURL()) 
-        let mutedlogembed = new Discord.MessageEmbed()
-        .setColor("#0084ff")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({
-          dynamic: true
-        }))
-        .setTitle(`Mute Issued`)
-        .setDescription(`Sucessfully issued a mute to <@${member.user.id}>.`)
-        .addFields(
-		  { name: 'Reason', value: `${reason}`, inline: true },
-		  { name: 'Moderator', value: `<@${message.author.id}>`, inline: true },
-	    )
-        .setTimestamp()
-        .setFooter(client.user.username, client.user.displayAvatarURL()) 
-
-        member.roles.add(muterole.id, reason).catch(console.error).then(() => {
-            message.channel.send(mutedlogembed)
-            return client.channels.resolve('709074878912790529').send(mutedembed)
-        })
+	const member = message.guild.member(message.mentions.user.first() || message.guild.members[1]))
+	if (!member) return
+	
+	let muterole = message.guild.roles.cache.find(r => r.name === "Muted");
+	if (!muterole) return message.reply("mr nil"
+	
+	let time = args[2];
+	if (!time){
+		return message.reply("time nil")
+	 }
+	    
+	 member.roles.add(muterole.id)
+	    
+	message.channel.send(`@${member.user.tag} has now been muted for ${ms(ms(time))}`};
+	    
+	 setTimeout(function(){
+		member.roles.remove(muterole.id);
+		message.channel.send(`nmmuted`
+	  }, ms(time));
+	    
+	    
+	    break;
+	    
   }
 }; 
