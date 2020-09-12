3c1bfa3790d136c8d748f2271a96582a50718d22
_Pear <pearofficialyt@gmail.com>
2020-09-12 13:20:50 -0400

    Update bot-start.bat

--- bot-start.bat
+++ bot-start.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 TITLE WaterstonSystems Console
 CLS
+ECHO [Using Windows]
 ECHO Starting WaterstonSystems...
 ECHO Close the window to safely terminate the process.
 ECHO.
 NPM start
+REM Should the bot crash, the PAUSE option will allow you to view the logs instead of abruptly closing.
 PAUSE
