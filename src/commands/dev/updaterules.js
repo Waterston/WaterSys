cf5e9fc50454708dcee1a843bd55a9aa7e79fc4d
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-09-11 23:46:14 -0400

    Update updaterules.js

--- src/commands/dev/updaterules.js
+++ src/commands/dev/updaterules.js
@@ -14,14 +14,14 @@ module.exports = {
 	    
 	 let ure1 = new Discord.MessageEmbed()
         .setColor("#0084ff")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({
-          dynamic: true
-        }))
         .setImage(`https://media.discordapp.net/attachments/746430272224100522/754133120189464616/image0.png?width=1362&height=504`)
-        .setDescription(`**Welcome to the official Baylor County, Waterston!\n\nThe State of Waterston was founded and created by Bandelqs and Pearism on Roblox.\n\n\nThe rules are subject to change at any time. By using the server you acknowledge and agree to following all rules listed below.`)
-        .setTimestamp()
+	 
+	let ure2= new Discord.MessageEmbed()
+        .setColor("#0084ff")
+        .setDescription(`**Welcome to the official Baylor County, Waterston!**\n\n**The State of Waterston was founded and created by Bandelqs and Pearism on Roblox.**\n\n\n***The rules are subject to change at any time. By using the server you acknowledge and agree to following all rules listed below.***`)
      
          client.channels.resolve('709052688368664688').send(ure1)
+	 client.channels.resolve('709052688368664688').send(ure2)
 	   
   }
 }; 
