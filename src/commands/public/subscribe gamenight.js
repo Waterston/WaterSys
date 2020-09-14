1a56dd68349f22168c780db8f5ccf234c1c0397f
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-09-13 20:37:08 -0400

    Create subscribe gamenight.js

---
+++ src/commands/public/subscribe gamenight.js
@@ -0,0 +1,29 @@
+const { stripIndents } = require('common-tags');
+let Discord = require('discord.js')
+
+module.exports = {
+    name: "subscribe gamenight",
+    category: "moderation",
+	description: "Mutes a user",
+	guildOnly: true,
+	usage: "<mention, id>",
+    run: async (client, message, args) => {
+
+	let muterole = message.guild.roles.cache.find(r => r.name === "Gamenight Subscriber");
+	if (!muterole) return message.reply("r nil")
+	    
+	 let gnsubembed = new Discord.MessageEmbed()
+        .setColor("#0084ff")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({
+          dynamic: true
+        }))
+        .setTitle(`Added Subscription`)
+        .setDescription(`Sucessfully added your subscription to **Gamenight Subscriber**`)
+        .setTimestamp()
+        .setFooter(client.user.username, client.user.displayAvatarURL()) 
+
+	    
+	 member.roles.add(muterole.id, reason)
+	 message.channel.send(gnsubembed)
+  }
+}; 
