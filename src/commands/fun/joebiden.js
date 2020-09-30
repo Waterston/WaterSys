69778002d174be77ec44ad38e25fa3a522df0c46
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-09-30 10:46:06 -0400

    Update joebiden.js

--- src/commands/fun/joebiden.js
+++ src/commands/fun/joebiden.js
@@ -7,8 +7,8 @@ module.exports = {
     description: "Friendly version of debate",
     usage: "<mention, id>",
     run: async (client, message, args) => {
-    if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ You're a **LIAR!**`).then(r => r.delete({timeout: 10000}))
-        message.channel.send("Will you shut up, man?)
+    if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send("https://cdn.discordapp.com/attachments/746430272224100522/760874849601716240/image0.png").then(r => r.delete({timeout: 10000}))
+        message.channel.send("Will you shut up, man?")
         message.channel.send("https://cdn.discordapp.com/attachments/739148349722198016/760874426492518400/unknown.png")
     }
 }
