b42772d23dfc13a0e5c002ff94e07d089cbaeb1a
WaterstonState <59323895+WaterstonState@users.noreply.github.com>
2020-09-30 10:43:53 -0400

    Create joebiden.js

---
+++ src/commands/fun/joebiden.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "joebiden",
+    aliases: ['joe', 'jb'],
+    category: "fun",
+    description: "Friendly version of debate",
+    usage: "<mention, id>",
+    run: async (client, message, args) => {
+    if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ You're a **LIAR!**`).then(r => r.delete({timeout: 10000}))
+        message.channel.send("Will you shut up, man?)
+        message.channel.send("https://cdn.discordapp.com/attachments/739148349722198016/760874426492518400/unknown.png")
+    }
+}
