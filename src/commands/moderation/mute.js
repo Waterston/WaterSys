5adc39464e15ebf9fb1e53f4ffe349d22211cf40
_Pear <pearofficialyt@gmail.com>
2020-10-14 13:15:14 -0400

    Improved error message

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -8,24 +8,24 @@ module.exports = {
 	description: "Mutes a user",
 	guildOnly: true,
 	usage: "<mention, id>",
+	
     run: async (client, message, args) => {
         if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
-    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
-          timeout: 10000
-       }))
+    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({timeout: 10000}))
        let member = message.mentions.members.first()
 
         if (message.author.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
+	if (client.user.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on the bot.`).catch(console.error);
 	    
 	let reason = args.slice(2).join(" ");
-        if(!reason) reason = "No reason given"
+        if(!reason) reason = "No reason specified"
 	
 	let muterole = message.guild.roles.cache.find(r => r.name === "Muted");
-	if (!muterole) return message.reply("mr nil")
+	if (!muterole) return message.reply(":warning: An error occurred in finding `Muted` role. Please contact a server administrator.")
 	
 	let time = args[1];
 	if (!time){
-		return message.reply("time nil")
+		return message.reply(":warning: Invalid time.")
 	 }
 	    
 	 let mutedembed = new Discord.MessageEmbed()
