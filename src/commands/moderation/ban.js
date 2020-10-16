40099aaafd7055f86b149e9228fd33557d8c0ac8
_Pear <pearofficialyt@gmail.com>
2020-10-16 13:01:18 -0400

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -10,15 +10,15 @@ module.exports = {
   usage: "<mention, id>",
   run: async (client, message, args) => {
     if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
-    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({
-      timeout: 10000
-    }))
-    let member = message.mentions.members.first()
+    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({timeout: 10000}))
+    let member = msssage.mentions.members.first() || message.guild.members.get(args[0]);
     let reason = args.slice(1).join(' ')
     if (reason.replace(/ /g, '').trim() === '') reason = `No reason specified`
     if (message.author.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
     if (client.user.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on the bot.`).catch(console.error);
+    .catch(err => { return message.channel.send(`⛔ An error occurred in banning the user. No action was taken.`)});
     await member.ban()
+
     let blogEmbed = new Discord.MessageEmbed()
       .setColor("#0084ff")
       .setAuthor(message.author.tag, message.author.displayAvatarURL({
