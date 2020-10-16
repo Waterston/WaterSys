3592811d2cfcd2f0e348783209d6017283f845c7
_Pear <pearofficialyt@gmail.com>
2020-10-16 13:52:48 -0400

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -7,17 +7,18 @@ module.exports = {
   category: "Moderation",
   description: "Bans a user from the server",
   guildOnly: true,
-  usage: "<mention, id>",
+  usage: "<mention, id> <reason>",
   run: async (client, message, args) => {
-    if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`⛔ Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
-    if (message.mentions.members.size === 0) return message.channel.send(`⚠️ No user specified, please mention the user.`).then(r => r.delete({timeout: 10000}))
+    if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send(`:no_entry: Insufficient permissions.`).then(r => r.delete({timeout: 10000}))
+    if (message.mentions.members.size === 0) return message.channel.send(`:warning: No user specified, please mention the user.`).then(r => r.delete({timeout: 10000}))
     let member = message.guild.members.first() || message.guild.members.get(args[0]);
     let reason = args.slice(1).join(' ')
     if (reason.replace(/ /g, '').trim() === '') reason = `No reason specified`
-    if (message.author.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on yourself.`)
-    if (client.user.id === member.user.id) return message.channel.send(`⛔ You cannot run this command on the bot.`).catch(console.error);
+    if (message.author.id === member.user.id) return message.channel.send(`:no_entry: You cannot run this command on yourself.`)
+    if (client.user.id === member.user.id) return message.channel.send(`:no_entry: You cannot run this command on the bot.`).catch(console.error);
     await member.ban()
 
+    // Builds the embed for mod logs channel	  
     let blogEmbed = new Discord.MessageEmbed()
       .setColor("#0084ff")
       .setAuthor(message.author.tag, message.author.displayAvatarURL({
@@ -31,6 +32,7 @@ module.exports = {
 	    )
       .setTimestamp()
       .setFooter(client.user.username, client.user.displayAvatarURL()) 
+    // Notifies the moderator that the command was executed
     let banembed = new Discord.MessageEmbed()
       .setColor("#0084ff")
       .setAuthor(message.author.tag, message.author.displayAvatarURL({
