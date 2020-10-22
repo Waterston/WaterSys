b15b83da97631e6b208c8efa440dcffdce992f42
TheRealPear <pearofficialyt@gmail.com>
2020-10-22 14:08:13 -0400

    Update whois.js

--- src/commands/verification/whois.js
+++ src/commands/verification/whois.js
@@ -10,10 +10,12 @@ module.exports = {
     guildOnly: true,
     run: async (client, message, args) => {
 
+        //let member = message.mentions.members.first() || message.member
         // Sets the variables to the API links.
         let body = await fetch(`https://verify.eryn.io/api/user/${message.author.id}`).then(res => res.json())
         let StatusBio = await fetch(`https://users.roblox.com/v1/users/${body.robloxId}/status`).then(res => res.json())
         let JoinDate = await fetch(`https://users.roblox.com/v1/users/${body.robloxId}`).then(res => res.json())
+        let RobloxAvatar = await getAccountThumbnail(body.robloxId)
 
         // Define the first embed, which will contain Roblox info.
         let whoembed = new Discord.MessageEmbed()
@@ -24,13 +26,16 @@ module.exports = {
         .setColor("#0084ff")
         .setAuthor(message.author.tag, message.author.displayAvatarURL())
         .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setThumbnail(RobloxAvatar)
         .setTimestamp()
-        
+
         message.channel.send(whoembed)
     }
 }
 
-
-
-
-
+async function getAccountThumbnail(id) {
+    let {
+      body
+    } = await fetch.get(`https://thumbnails.roblox.com/v1/users/avatar?userIds=${id}&size=352x352&format=Png&isCircular=false`)
+    return body.data[0].imageUrl
+  }
