39ba7bf0016907b01c2522eb8b8f7b63ca00c35a
TheRealPear <pearofficialyt@gmail.com>
2020-10-22 13:18:53 -0400

    Update whois.js

--- src/commands/verification/whois.js
+++ src/commands/verification/whois.js
@@ -5,19 +5,24 @@ const fetch = require('node-fetch')
 module.exports = {
     name: "whois",
     aliases: ['roblox', 'info'],
-    category: "Roblox",
-    description: "stalk ppl",
+    category: "info",
+    description: "Identify and retrive the specified user's information",
+    guildOnly: true,
     run: async (client, message, args) => {
 
-        //Get Response of Body
+        // Sets the variables to the API links.
         let body = await fetch(`https://verify.eryn.io/api/user/${message.author.id}`).then(res => res.json())
         let StatusBio = await fetch(`https://users.roblox.com/v1/users/${body.robloxId}/status`).then(res => res.json())
         let JoinDate = await fetch(`https://users.roblox.com/v1/users/${body.robloxId}`).then(res => res.json())
 
-        //Create Sending Embed Completion
-        const whoembed = new Discord.MessageEmbed()
-        .setTitle(`Roblox Username: ${body.robloxUsername}`)
-        .setDescription(`**Status:**\n${StatusBio.status}\n\n**Creation Date:**\n${JoinDate.created}`)
+        // Define the first embed, which will contain Roblox info.
+        let whoembed = new Discord.MessageEmbed()
+        .setTitle(`${body.robloxUsername}`)
+        .addField(
+            { name: 'Status', value: `${StatusBio.status}`, inline: false },
+            { name: 'Creation Date', value: `${JoinDate.created}`, inline: false },
+        )
+        //.setDescription(`**Status:**\n${StatusBio.status}\n\n**Creation Date:**\n${JoinDate.created}`)
         .setColor("#0084ff")
         .setAuthor(message.author.tag, message.author.displayAvatarURL())
 
