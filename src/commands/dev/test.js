b67e71c557f8dde20c677390075c93c53f0dc66c
TheRealPear <pearofficialyt@gmail.com>
2020-11-12 16:28:43 -0500

    Create test.js

---
+++ src/commands/dev/test.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "test",
+    aliases: ['testing', 'base'],
+    category: "dev",
+    description: "Test command",
+    usage: "<mention, id>",
+    ownerOnly: true,
+    run: async (client, message, args) => {
+        message.channel.send("Test command.")
+    }
+}
