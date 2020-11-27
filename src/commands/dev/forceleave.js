541c8a171d434362c270564ddd95cbf23c9530e0
usernameismine <61718525+usernameismine@users.noreply.github.com>
2020-11-26 19:42:51 -0500

    Update forceleave.js
    yeah duh

--- src/commands/dev/forceleave.js
+++ src/commands/dev/forceleave.js
@@ -7,22 +7,22 @@ module.exports = {
     category: "dev",
     hidden: true,
 	  description: "Forces the bot to leave the current or a specified server",
-    guildOnly: false,
+    guildOnly: true,
     ownerOnly: true,
     usage: "<id>",
     run: async (client, message, args) => {
         let guildID = args[0]
-      	if (message.author.id !== config.ownerID) return console.log(`${message.author.tag} attempted to force-leave unsuccessfully.`)
+        
         if (!guildID){ return message.reply(":warning: No guild ID specified, please supply a valid ID.").then(msg => msg.delete({timeout: 10000}))}
 
-        const guild = bot.guilds.cache.find((g) => g.id === guildId);
+        const guild = client.guilds.cache.find((g) => g.id === guildID);
         if (!guild) { return message.channel.send("The guild ID you supplied does not exist.").then(msg => msg.delete({timeout: 10000}))}
 
         let confirmationEmbed = new Discord.MessageEmbed()
         .setColor("#be1931")
         .setTitle("Force Leave")
         .setAuthor(message.author.tag, message.author.displayAvatarURL({timeout: 10000}))
-        .setDescription(`Are you sure you want to force leave guild ${g}? This will remove the bot from the specified guild.`)
+        .setDescription(`Are you sure you want to force leave guild ${guild.name}? This will remove the bot from the specified guild.`)
         .setTimestamp()
         let msg = await message.channel.send(confirmationEmbed)
         await msg.react('✅')
