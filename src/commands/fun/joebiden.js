97f2fabb8f658ad13d4d6243f4df031f92d61d02
_Pear <pearofficialyt@gmail.com>
2021-02-14 22:45:48 -0500

    Update joebiden.js

--- src/commands/fun/joebiden.js
+++ src/commands/fun/joebiden.js
@@ -4,8 +4,7 @@ module.exports = {
     name: "joebiden",
     aliases: ['joe', 'jb'],
     category: "fun",
-    description: "Friendly version of debate",
-    usage: "<mention, id>",
+    description: "For when you need to Joe Biden.",
     run: async (client, message, args) => {
     if (!message.member.roles.cache.get('709047575180869663')) return message.channel.send("https://cdn.discordapp.com/attachments/746430272224100522/760874849601716240/image0.png").then(r => r.delete({timeout: 10000}))
         message.channel.send("Will you shut up, man?")
