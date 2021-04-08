da4af2a80bbbfebd6d3cb87d69f0ca4fbd40b548
TheRealPear <pearofficialyt@gmail.com>
2021-04-07 23:17:21 -0400

    more attempt at id mentions

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -8,7 +8,7 @@ module.exports = {
   usage: "<mention, id> <reason>",
   run: async (client, message, args) => {
     if (!message.member.roles.cache.some(role => role.name === 'Discord Moderator')) return; //message.channel.send(`⛔ Insufficient permissions to run this command.`).then(r => r.delete({timeout: 10000}))
-    let member = message.mentions.members.first() || message.guild.members.fetch(args[0]);
+    let member = message.mentions.members.first() || await message.guild.members.fetch(args[0]);
     if (!member){ return message.channel.send(`:warning: No user specified, please mention a user or provide a valid ID.`).then(msg => msg.delete({ timeout: 10000 }))}
     let reason = args.slice(1).join(' ')
     if(!reason) reason = `No reason specified`
