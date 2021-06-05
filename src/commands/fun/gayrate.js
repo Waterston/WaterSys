84315edd6da764ff81e059c44deb0292d5f6b6e0
Pear <pearofficialyt@gmail.com>
2021-06-05 17:29:16 -0400

    rigged command

--- src/commands/fun/gayrate.js
+++ src/commands/fun/gayrate.js
@@ -9,12 +9,19 @@ module.exports = {
     guildOnly: true,
     run: async (client, message, args) => {
         let member = message.mentions.members.first() || message.member
+        let rate = `${Math.floor(Math.random() * 101)}% gay`
+
+        // Rig the ratings for specific users.
+        if (member.user.id === "137663615657312256") { rate = "[redacted]" } // Pear
+        if (member.user.id === "668529679732572170") { rate = "101% gay" } // RGB_Flames
+        if (member.user.id === "213702181533122560") { rate = "100% gay, but he's not ready to accept that yet." } // Toboo_Racc
+        if (member.user.id === "341022841405308931") { rate = "200% gay" } // bowsercube
+
         const embed = new Discord.MessageEmbed()
         .setColor("#0084ff")
         .setAuthor(message.author.tag, message.author.displayAvatarURL())
         .setTitle(`Gay Rate`)
-        .setDescription(`<@${member.user.id}> is ${Math.floor(Math.random() * 101)}% gay. :rainbow_flag:`)
-        .addField("Disclaimer", "We do not allow harassment based on someone's sexual orentation. If someone is harassing you or someone else, let a moderator know.", false)
+        .setDescription(`<@${member.user.id}> is ${rate}. :rainbow_flag:`)
         .setFooter(client.user.username, client.user.displayAvatarURL())
         .setTimestamp()
         message.channel.send(embed)
